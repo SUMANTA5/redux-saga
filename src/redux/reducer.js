e93a3c009273e2076749b92b1233ada61af11686
SUMANTA5 <sumanta111195@gmail.com>
2022-08-18 09:42:36 +0530

    switch case

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,11 +1,11 @@
-import {ADD_TO_CART} from './constant'
+import { ADD_TO_CART } from "./constant";
 export const cartData = (data = [], action) => {
-    
-    if(action.type === ADD_TO_CART){
-        console.log("reducx call",action);
-        return action.data
-    }else {
-        return "no action matched"
-    }
-    
- }
+  switch (action.type) {
+    case ADD_TO_CART:
+      console.log("reducx call", action);
+      return 1 + 1;
+
+    default:
+      return "no action matched";
+  }
+};
