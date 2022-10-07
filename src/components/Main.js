ff7f132a2b04327fa536a164a23eb50a9ee0f971
SUMANTA KUMAR DAS <48908320+SUMANTA5@users.noreply.github.com>
2022-10-07 14:17:01 +0530

    item

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import { addToCart, emptyCart, removeFromCart } from "../redux/action";
 import { useDispatch } from "react-redux";
 import { productList } from "../redux/productAction";
 import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
 
 function Main() {
   const dispatch = useDispatch();
@@ -10,31 +11,36 @@ function Main() {
 
   console.log("data in main comp.....", data);
 
-  const product = {
-    itemName: "i phone",
-    type: "Mobile",
-    price: "100000",
-    color: "red",
-  };
+  useEffect(() => {
+    dispatch(productList());
+  }, []);
+
   return (
     <div>
-      <div>
-        <button onClick={() => dispatch(addToCart(product))}>
-          Add To Cart
-        </button>
-      </div>
-      <div>
-        <button onClick={() => dispatch(removeFromCart(product.itemName))}>
-          Remove from Cart
-        </button>
-      </div>
-      <div>
+      <div className="product-card">
         <button onClick={() => dispatch(emptyCart())}>Empty Cart</button>
       </div>
-      <div>
-        <button onClick={() => dispatch(productList())}>
-          gat product List
-        </button>
+      <div className="product-class">
+        {data.map((item) => (
+          <div key={item.id} className="product-item">
+            <img src={item.img} alt="" />
+            <div>Name: {item.name}</div>
+            <div>Color: {item.color}</div>
+            <div>Price: {item.price}</div>
+            <div>Category: {item.category}</div>
+            <div>Brand: {item.brand}</div>
+            <div>
+              <button onClick={() => dispatch(addToCart(item))}>
+                Add To cart
+              </button>
+            </div>
+            <div>
+              <button onClick={() => dispatch(removeFromCart(item.id))}>
+                Remove from Cart
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/redux/productSage.js
+++ src/redux/productSage.js
@@ -2,7 +2,7 @@ import { takeEvery, put } from "redux-saga/effects";
 import { PRODUCT_LIST, SET_PRODUCT_LIST } from "./constant";
 
 function* getProduct() {
-  let data = yield fetch("http://localhost:4000/product");
+  let data = yield fetch("http://localhost:5000/product");
   data = yield data.json();
   yield put({ type: SET_PRODUCT_LIST, data });
 }
