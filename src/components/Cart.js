87687c183c8157fc8ee269ebf38b894e9c0af16c
SUMANTA KUMAR DAS <48908320+SUMANTA5@users.noreply.github.com>
2022-10-10 13:04:28 +0530

    Route

---
+++ src/components/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {} from 'react-router-dom'
+
+const Cart = () => {
+  return (
+    <div>
+      <h1>Cart</h1>
+    </div>
+  )
+}
+
+export default Cart
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
+import { Link } from 'react-router-dom'
+import "../App.css";
 
 const Header = () => {
     const result = useSelector((state) => state.cartData)
   return (
     <div className='header'>
+      <Link to='/'>
+      <h1 className='logo'>E-com</h1>
+      </Link>
+        <Link to='/cart'>
         <div className='cart-div'>
             <span>{result.length}</span>
             <img src='https://cdn-icons-png.flaticon.com/512/263/263142.png' alt='' />
 
         </div>
+        </Link>
       
     </div>
   )
